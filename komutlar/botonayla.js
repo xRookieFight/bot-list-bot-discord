1fadb63a1f4fe878a1233cddfe36badcb8c32d61
Mert Yıldırım <50714128+xRookieFight@users.noreply.github.com>
2020-08-15 18:27:49 +0300

    Update botonayla.js

--- komutlar/botonayla.js
+++ komutlar/botonayla.js
@@ -1,13 +1,11 @@
 const config = require('../config.json')
 exports.run = (client, message, args) => {
-  if (message.author.hasPermissions('MANAGE_MESSAGES')) return;
   let sahipid = args.slice(0).join(' ');
   let botid = args.slice(1).join(' ');
   if (sahipid.length < 1) return message.reply('Bir sahip id yazmalısın.');
   if (botid.length < 1) return message.reply('Bir bot id yazmalısın.');
 message.react('✅')
-client.channels.get(config.log).send(`<@${sahipid}> adlı kişinin <@${botid} adlı botu onaylandı! :white_check_mark:`)
-client.users.get(sahipid).send(`<@${botid}> adlı botunuz onaylandı! :tada:`)
+client.channels.get(config.log).send(`<@${sahipid}> adlı kişinin <@${botid}> adlı botu onaylandı! :white_check_mark:`)
 }
 exports.conf = {
 enabled: true,
